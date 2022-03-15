4de6e9f8d204667c061c89bc80c15a42d9e4ad28
leon <757365800@qq.com>
2022-03-16 00:30:09 +0800

    update 6.0.5-beta.6

--- src/Leon.Live.API/Program.cs
+++ src/Leon.Live.API/Program.cs
@@ -1,13 +1,11 @@
-using Serilog;
 
 Environment.SetEnvironmentVariable("ASPNETCORE_HOSTINGSTARTUPASSEMBLIES", "NetPro.Startup");
 var host = Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((hostingContext, config) => ApolloClientHelper.ApolloConfig(hostingContext, config, args))
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.ConfigureKestrel(options =>
                     {
                     });
-                }).UseSerilog();
+                });
 
 host.Build().Run();
